d1c64d59c4c478f4d901b78d47640dff729495bd
PurinPintakhiew <dev.purin@gmail.com>
2024-12-11 23:54:40 +0700

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useRoutes } from "react-router-dom";
+
+import Home from "./pages/Home";
+
+const App = () => {
+  const elements: any = [{ path: "/", element: <Home /> }];
+
+  return useRoutes(elements);
+};
+ 
+export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+const Home = () => {
+  return (
+    <>
+      <div className="container">
+        <div className="text-center mt-5">
+          <h1 className="text-4xl text-blue-400">เที่ยวไหนดี</h1>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Home;
