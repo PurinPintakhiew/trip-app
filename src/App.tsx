acdc4fc7bb1c14b2180ff1d647b265a72984a5e7
PurinPintakhiew <dev.purin@gmail.com>
2024-12-13 14:29:21 +0700

    feat : keyword path

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import { useRoutes } from "react-router-dom";
 import Home from "./pages/Home";
 
 const App = () => {
-  const elements: any = [{ path: "/", element: <Home /> }];
+  const elements: any = [
+    { path: "/", element: <Home /> },
+    { path: "/:keyword", element: <Home /> },
+  ];
 
   return useRoutes(elements);
 };
- 
+
 export default App;
