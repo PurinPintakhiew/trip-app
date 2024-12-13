cc8e62d579c1889678b34fa2ec691c1f33d04579
PurinPintakhiew <dev.purin@gmail.com>
2024-12-13 14:29:44 +0700

    chore : update filter

--- src/reducers/tripSlice.ts
+++ src/reducers/tripSlice.ts
@@ -1,12 +1,27 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const apiUrl = "http://localhost:9000";
+const apiUrl = import.meta.env.VITE_API_URL;
 
-export const fetchTrips = createAsyncThunk("trips/fetchTrips", async () => {
-  const response = await axios.get(apiUrl);
-  return response.data;
-});
+export const fetchTrips = createAsyncThunk(
+  "trips/fetchTrips",
+  async (q: any) => {
+    const response = await axios.get(apiUrl);
+
+    if (q) {
+      response.data = response.data.filter(
+        (trip: any) =>
+          trip.title.toLowerCase().includes(q.toLowerCase()) ||
+          trip.description.toLowerCase().includes(q.toLowerCase()) ||
+          trip.tags.some((tag: any) =>
+            tag.toLowerCase().includes(q.toLowerCase())
+          )
+      );
+    }
+
+    return response.data;
+  }
+);
 
 export interface TripSliceStateProps {
   trips: [];
